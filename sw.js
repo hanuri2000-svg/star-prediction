cec443913fd47a540281bcc630079e60bf251d3c
hanuri2000-svg <hanuri2000@naver.com>
2026-08-08 21:20:05 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const SW_VERSION='3.2.0';
+self.addEventListener('install',event=>self.skipWaiting());
+self.addEventListener('activate',event=>{
+  event.waitUntil((async()=>{
+    const keys=await caches.keys();
+    await Promise.all(keys.map(k=>caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET') return;
+  const url=new URL(req.url);
+  if(url.origin!==location.origin) return;
+  event.respondWith((async()=>{
+    try{
+      return await fetch(req,{cache:'no-store'});
+    }catch(e){
+      return fetch(req);
+    }
+  })());
+});
