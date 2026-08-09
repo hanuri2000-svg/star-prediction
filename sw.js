3986ab373b20278a39bcd119e0fd453e978b5a40
hanuri2000-svg <hanuri2000@naver.com>
2026-08-09 09:31:50 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-const SW_VERSION='3.5.0';
+const SW_VERSION='4.0.0';
 self.addEventListener('install',event=>self.skipWaiting());
 self.addEventListener('activate',event=>{
- event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));await self.clients.claim();})());
+ event.waitUntil((async()=>{
+  const keys=await caches.keys();
+  await Promise.all(keys.map(k=>caches.delete(k)));
+  await self.clients.claim();
+ })());
 });
 self.addEventListener('fetch',event=>{
  const req=event.request;if(req.method!=='GET')return;
